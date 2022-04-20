184eb3411ac5745690207b614a87282653512778
vinesaucebeep <88556555+vinesaucebeep@users.noreply.github.com>
2022-04-20 14:59:07 -0400

    Added blocks

--- src/main/java/simplicity/simplicity/core/init/BlockInit.java
+++ src/main/java/simplicity/simplicity/core/init/BlockInit.java
@@ -1,8 +1,7 @@
 package simplicity.simplicity.core.init;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,4 +14,8 @@ public class BlockInit {
 
     public static final RegistryObject<Block> EBONY_LOG = BLOCKS.register("ebony_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG).requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> STRIPPED_EBONY_LOG = BLOCKS.register("stripped_ebony_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> RUBY_ORE = BLOCKS.register("ruby_ore", () -> new OreBlock(BlockBehaviour.Properties.copy(Blocks.EMERALD_ORE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> DEEPSLATE_RUBY_ORE = BLOCKS.register("deepslate_ruby_ore",() -> new OreBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_EMERALD_ORE).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.EMERALD_BLOCK).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> RED_CORNFLOWER = BLOCKS.register("red_cornflower", () -> new FlowerBlock(MobEffects.DIG_SPEED,6, BlockBehaviour.Properties.copy(Blocks.CORNFLOWER).requiresCorrectToolForDrops()));
 }
--- src/main/java/simplicity/simplicity/core/init/ItemInit.java
+++ src/main/java/simplicity/simplicity/core/init/ItemInit.java
@@ -13,6 +13,13 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Simplicity.MOD_ID);
 
     public static final RegistryObject<Item> EBONY_LOG = ITEMS.register("ebony_log", () -> new BlockItem(BlockInit.EBONY_LOG.get(),new Item.Properties().tab(Simplicity.SIMPLICITY)));
+    public static final RegistryObject<Item> STRIPPED_EBONY_LOG = ITEMS.register("stripped_ebony_log", () -> new BlockItem(BlockInit.STRIPPED_EBONY_LOG.get(), new Item.Properties().tab(Simplicity.SIMPLICITY)));
+    public static final RegistryObject<Item> RUBY_ORE = ITEMS.register("ruby_ore", () -> new BlockItem(BlockInit.RUBY_ORE.get(),new Item.Properties().tab(Simplicity.SIMPLICITY)));
+    public static final RegistryObject<Item> DEEPSLATE_RUBY_ORE = ITEMS.register("deepslate_ruby_ore", () -> new BlockItem(BlockInit.DEEPSLATE_RUBY_ORE.get(),new Item.Properties().tab(Simplicity.SIMPLICITY)));
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().tab(Simplicity.SIMPLICITY)));
+    public static final RegistryObject<Item> RED_CORNFLOWER = ITEMS.register("red_cornflower", () -> new BlockItem(BlockInit.RED_CORNFLOWER.get(),new Item.Properties().tab(Simplicity.SIMPLICITY)));
+
+
 
 
 }
