eb283aa2aed5264febc3ca8e9d82be3375754f86
vinesaucebeep <88556555+vinesaucebeep@users.noreply.github.com>
2022-04-21 10:26:23 -0400

    Occlusion

--- src/main/java/simplicity/simplicity/core/init/BlockInit.java
+++ src/main/java/simplicity/simplicity/core/init/BlockInit.java
@@ -17,5 +17,5 @@ public class BlockInit {
     public static final RegistryObject<Block> RUBY_ORE = BLOCKS.register("ruby_ore", () -> new OreBlock(BlockBehaviour.Properties.copy(Blocks.EMERALD_ORE).requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> DEEPSLATE_RUBY_ORE = BLOCKS.register("deepslate_ruby_ore",() -> new OreBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_EMERALD_ORE).requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.EMERALD_BLOCK).requiresCorrectToolForDrops()));
-    public static final RegistryObject<Block> RED_CORNFLOWER = BLOCKS.register("red_cornflower", () -> new FlowerBlock(MobEffects.DIG_SPEED,6, BlockBehaviour.Properties.copy(Blocks.CORNFLOWER).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> RED_CORNFLOWER = BLOCKS.register("red_cornflower", () -> new FlowerBlock(MobEffects.DIG_SPEED,6, BlockBehaviour.Properties.copy(Blocks.CORNFLOWER).instabreak().noOcclusion()));
 }
