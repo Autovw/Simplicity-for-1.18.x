22d95a1d10b56fce2869040a55ff2efdee632ef5
Autovw <68228006+Autovw@users.noreply.github.com>
2022-04-21 15:03:45 +0200

    Add providers for generating models and blockstates

--- src/main/java/simplicity/simplicity/datagen/ModDataGenerator.java
+++ src/main/java/simplicity/simplicity/datagen/ModDataGenerator.java
@@ -1,4 +1,25 @@
 package simplicity.simplicity.datagen;
 
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import simplicity.simplicity.Simplicity;
+import simplicity.simplicity.datagen.provider.ModBlockStateProvider;
+import simplicity.simplicity.datagen.provider.ModItemModelProvider;
+
+@Mod.EventBusSubscriber(modid = Simplicity.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModDataGenerator {
+
+    @SubscribeEvent
+    public static void onGatherData(final GatherDataEvent event) {
+        DataGenerator dataGenerator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        if (event.includeClient()) {
+            dataGenerator.addProvider(new ModItemModelProvider(dataGenerator, Simplicity.MOD_ID, existingFileHelper));
+            dataGenerator.addProvider(new ModBlockStateProvider(dataGenerator, Simplicity.MOD_ID, existingFileHelper));
+        }
+    }
 }
